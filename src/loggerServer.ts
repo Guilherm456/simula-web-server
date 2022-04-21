0e4c61e53b597b446ad4bc216fa3e2fd30b3d4f9
Guilherme Vier <guilhermevier456@gmail.com>
2022-04-21 13:08:33 -0300

    Criação de um logger que salva em arquivo

---
+++ src/loggerServer.ts
@@ -0,0 +1,39 @@
+import { ConsoleLogger } from '@nestjs/common';
+import { appendFileSync, statSync, unlinkSync } from 'fs';
+
+type LogLevel = 'debug' | 'info' | 'warn' | 'error' | 'log';
+
+export class LoggerServer extends ConsoleLogger {
+  log(message: string) {
+    this.saveInFile(message, 'log');
+    super.log(message);
+  }
+  error(message: string, trace?: string) {
+    this.saveInFile(message, 'error');
+    super.error(message);
+  }
+  warn(message: string) {
+    this.saveInFile(message, 'warn');
+    super.warn(message);
+  }
+  debug(message: string) {
+    this.saveInFile(message, 'debug');
+    super.debug(message);
+  }
+
+  //Função para salvar logs no arquivo
+  saveInFile(message: string, type: LogLevel) {
+    const file = `${__dirname}/log.txt`;
+    const size = statSync(file).size;
+    //Se o arquivo for maior que 50MB, apaga o arquivo e recomeça
+    if (size >= 50000000) {
+      console.log('deletando arquivo');
+      unlinkSync(file);
+    }
+    const date = new Date().toLocaleString();
+    //Log no arquivo vai parecer como: data - [tipo] [mensagem]
+    const log = `${date} - [${type}] ${message} \n`;
+    //Adiciona ao arquivo
+    appendFileSync(file, log);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggerServer } from './loggerServer';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    //define o logger do server com o tipo LoggerServer (customizado)
+    logger: new LoggerServer(),
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
