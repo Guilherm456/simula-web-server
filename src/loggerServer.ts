184c94f89a79cdd8ccacb49370923b64c005334e
guilhermevier <guilhermevier456@gmail.com>
2024-03-25 10:42:26 -0300

    (fix) Corrigido a falta de criação do logger

--- src/loggerServer.ts
+++ src/loggerServer.ts
@@ -1,17 +1,19 @@
 import { ConsoleLogger } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 import {
   WriteStream,
   createWriteStream,
   existsSync,
   statSync,
   unlinkSync,
+  writeFileSync,
 } from 'fs';
 
 type LogLevel = 'debug' | 'info' | 'warn' | 'error' | 'log';
 
 export class LoggerServer extends ConsoleLogger {
   private logStream: WriteStream;
-  private readonly maxLogSize = 5000000; // 5MB
+  private readonly maxLogSize = 500 * 1024;
   private readonly logFile = `${__dirname}/log.json`;
 
   constructor() {
@@ -47,11 +49,14 @@ export class LoggerServer extends ConsoleLogger {
   }
 
   private initializeLogStream() {
+    if (!existsSync(this.logFile)) {
+      writeFileSync(this.logFile, '', { flag: 'w' });
+    }
     this.logStream = createWriteStream(this.logFile, { flags: 'a' });
   }
 
   private checkAndRotateLogFile() {
-    const size = statSync(this.logFile).size;
+    const size = statSync(this.logFile)?.size;
     if (size >= this.maxLogSize) {
       this.logStream.end(); // Fecha o stream atual
       this.rotateLogFileIfNeeded(); // Rotaciona o arquivo
@@ -62,7 +67,15 @@ export class LoggerServer extends ConsoleLogger {
   saveInFile(message: string | string[], type: LogLevel) {
     this.checkAndRotateLogFile();
     const date = new Date().toISOString();
-    const logEntry = JSON.stringify({ date, type, message }) + '\n';
+    const logEntry =
+      JSON.stringify({
+        date,
+        type,
+        message:
+          typeof message === 'string' ? message : JSON.stringify(message),
+
+        id: randomUUID(),
+      }) + '\n';
     this.logStream.write(logEntry);
   }
 }
