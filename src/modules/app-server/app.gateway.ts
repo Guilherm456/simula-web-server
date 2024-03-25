72ac6fcedfa7183c9d3ff5c0135a10d2670516d3
guilhermevier <guilhermevier456@gmail.com>
2024-03-25 10:41:29 -0300

    (change) Melhorado validação do arquivo de log

--- src/modules/app-server/app.gateway.ts
+++ src/modules/app-server/app.gateway.ts
@@ -7,13 +7,15 @@ import {
 } from '@nestjs/websockets';
 import { existsSync, openSync, readFileSync, watchFile } from 'fs';
 
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import { LoggerServer } from 'src/loggerServer';
 
 @WebSocketGateway({ cors: '*', path: '/log' })
 @Injectable()
 export class AppGateway implements OnGatewayInit {
   private file: string;
+  private lastLineSent = 0;
+  private totalLines = 0;
 
   constructor(private logger: LoggerServer) {}
 
@@ -29,7 +31,10 @@ export class AppGateway implements OnGatewayInit {
     }
     try {
       watchFile(this.file, () => {
-        this.server.emit('log', this.handleLog());
+        this.server.emit(
+          'newLog',
+          this.handleLog({ isFirstConnection: false }),
+        );
       });
     } catch (e) {
       this.logger.error('Algum erro ocorreu no sistema de arquivos do log');
@@ -37,12 +42,32 @@ export class AppGateway implements OnGatewayInit {
   }
 
   @SubscribeMessage('log')
-  handleLog(): object[] {
-    const lines = readFileSync(this.file)
-      .toString()
-      .split('\n')
-      .filter((line) => line);
+  handleSendLog(
+    client: Socket,
+    payload: { isFirstConnection: boolean },
+  ): object[] {
+    return this.handleLog(payload);
+  }
 
-    return lines.map((line) => JSON.parse(line));
+  handleLog(payload: { isFirstConnection: boolean }): object[] {
+    const lines = readFileSync(this.file).toString().split('\n');
+
+    if (lines.length < this.totalLines) {
+      // Se o número de linhas diminuiu, o arquivo foi rotacionado
+      this.lastLineSent = 0;
+    }
+    this.totalLines = lines.length;
+
+    let newLines = [];
+    if (payload.isFirstConnection) {
+      // Se for a primeira conexão do cliente, enviar todas as linhas
+      newLines = lines.filter((line) => line);
+    } else {
+      // Senão, enviar apenas as novas linhas
+      newLines = lines.slice(this.lastLineSent).filter((line) => line);
+    }
+    this.lastLineSent = lines.length - 1;
+
+    return newLines.map((line) => JSON.parse(line)).reverse();
   }
 }
